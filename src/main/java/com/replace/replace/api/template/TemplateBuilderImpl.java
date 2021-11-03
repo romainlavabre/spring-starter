fa4c604ff10e51a4bdc83fb3791fd067c36a004e
romainlavabre <romainlavabre98@gmail.com>
2021-11-03 21:19:44 +0100

    FIX TemplateBuilder.class

--- src/main/java/com/replace/replace/api/template/TemplateBuilderImpl.java
+++ src/main/java/com/replace/replace/api/template/TemplateBuilderImpl.java
@@ -5,6 +5,8 @@ import com.replace.replace.configuration.environment.Variable;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.io.StringWriter;
@@ -26,6 +28,9 @@ public class TemplateBuilderImpl implements TemplateBuilder {
     ) {
         this.environment    = environment;
         this.velocityEngine = new VelocityEngine();
+        this.velocityEngine.setProperty( RuntimeConstants.RESOURCE_LOADER, "classpath" );
+        this.velocityEngine.setProperty( "classpath.resource.loader.class", ClasspathResourceLoader.class.getName() );
+        this.velocityEngine.init();
     }
 
 
